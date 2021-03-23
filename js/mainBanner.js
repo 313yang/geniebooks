9defca6f5355e60017642f028244bcb032b1ef33
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-03-23 19:55:06 +0900

    update slider~~🎆

--- js/mainBanner.js
+++ js/mainBanner.js
@@ -1,3 +1,40 @@
-$(document).ready(function(){
-  $('.single-item').slick();
-});
+const firstSlide = document.querySelector(".slider__item:first-child");
+const lastSlide = document.querySelector(".slider__item:last-child");
+const leftBtn = document.querySelector(".js-leftBtn");
+const rightBtn = document.querySelector(".js-rightBtn");
+
+const SHOWING_CLASS = "showing";
+
+const slide =() =>{
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  if(currentSlide){
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const nextSlide = currentSlide.nextElementSibling;
+    if(nextSlide){
+      nextSlide.classList.add(SHOWING_CLASS);
+    } else{
+      firstSlide.classList.add(SHOWING_CLASS);
+    }
+  }
+};
+
+const handleSlide = () =>{
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  if(currentSlide){
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const prevSlide = currentSlide.previousElementSibling;
+    if(prevSlide){
+      prevSlide.classList.add(SHOWING_CLASS);
+    } else{
+      lastSlide.classList.add(SHOWING_CLASS);
+    }
+  }
+
+};
+
+function init(){
+  rightBtn.addEventListener("click", slide);
+  leftBtn.addEventListener("click", handleSlide);
+  setInterval(slide, 5000);
+};
+init();
